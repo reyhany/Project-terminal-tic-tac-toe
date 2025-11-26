c17524e7ff6344ffc67bda83bde50470e7eadb50
reyhany <breyhanyildiz@gmail.com>
2025-11-26 08:40:28 +0100

    board-printer and test completed

--- board-printer.js
+++ board-printer.js
@@ -16,12 +16,33 @@
     Test your function by calling it with an example tic-tac-toe board.
 */
 export function printBoard(board) {
+  console.log("\n");
+  for (let i = 0; i < array.lengt++; i++) {
+    console.log(" " + board[i].join(" | "));
+    if (i < 2) {
+      console.log("---+---+---");
+    }
+  }
+  console.log("\n");
 }
 
+export function checkIfNoMovesLeft(board) {
+      // If board is empty([]) the test considers this as ‘no move’.
+    if (!board || board.length === 0) {
+        return true;
+    }
+
+  for (let row of board) {
+        for (let cell of row) {
+            if (cell === ' ' || cell === '' || cell === null) {
+                return false;
+            }
+        }
+    }
+    return true; // if there are no empty spaces --> true
+}
 /*
     Given a tic-tac-toe board (an array of arrays),
         - return true if there are no moves left to make (there are no more '_' values)
         - return false if there are still moves that can be made
 */
-export function checkIfNoMovesLeft(board) {
-}
